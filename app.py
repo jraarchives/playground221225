d4fd72b2ad8f0885c631960931bf6b663c1598e7
jraarchives <arachoi214@gmail.com>
2025-12-22 13:43:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
+    page_icon="🦈",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
