ce7206be183dc4626cdaf7f0afe777818df940ae
jraarchives <arachoi214@gmail.com>
2025-12-22 13:40:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.write("Hello, *World!* :sunglasses:")
+# st.write("Hello, *World!* :sunglasses:")
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
